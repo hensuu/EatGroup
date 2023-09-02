3ff65eaa86dedc97ba12889451a3a1059fc4617d
hensuu <hensuu@csie.org>
2023-09-03 00:58:59 +0800

    Initialize

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,7 @@
+<script>
+  import "../app.postcss";
+</script>
+
+<div class="">
+<slot />
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<div class="navbar bg-base-100">
+  <a href="#" class="btn btn-ghost normal-case text-xl">EatGroup</a>
+</div>
+<div>
+</div>
